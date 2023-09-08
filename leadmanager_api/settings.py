a1c600c26183c30bcd3db332a913a03c7d573af8
AneeshNi47 <me@aneeshbharath.com>
2023-09-08 13:02:55 +0400

    added redis details

--- leadmanager_api/settings.py
+++ leadmanager_api/settings.py
@@ -106,7 +106,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [(config('REDIS_URL', default='127.0.0.1'), config('REDIS_PORT', default=6379))],
         },
     },
 }
